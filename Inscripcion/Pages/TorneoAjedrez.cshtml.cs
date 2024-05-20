c8552ffff7cdbf54624b3d1db2e80f8d07d1132e
Aquiles0603 <maximogomez370@gmail.com>
2024-05-20 13:29:39 -0300

    inicio del proyecto

---
+++ Inscripcion/Pages/TorneoAjedrez.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Inscripcion.Pages
+{
+    public class TorneoAjedrezModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
